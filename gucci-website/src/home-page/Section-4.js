58cdcd567280ab20d6af32822902844997221d05
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-03 15:15:29 +0100

    Section 4 styling

--- gucci-website/src/home-page/Section-4.js
+++ gucci-website/src/home-page/Section-4.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styling/section-four.css"
 import gucciImage from "../model/oladimeji-odunsi-Wu3yqve2gnc-unsplash.jpg"
+import gucciImage2 from "../model/calicadoo-ttdio_nOPjQ-unsplash.jpg"
+import gucciImage3 from "../model/isco--UfV80lOCqg-unsplash.jpg"
 
 function SectionFour(){
   return (
@@ -12,11 +14,11 @@ function SectionFour(){
           <p className="column-paragraph">GUCCI GOOD GAME COLLECTION</p>
         </div>
         <div className="column">
-          <img className="column-image" src={gucciImage} alt="gucci img" />
+          <img className="column-image" src={gucciImage2} alt="gucci img" />
           <p className="column-paragraph">GUCCI HA HA HA</p>  
         </div>
         <div className="column">
-          <img className="column-image" src={gucciImage} alt="gucci img" />
+          <img className="column-image" src={gucciImage3} alt="gucci img" />
           <p className="column-paragraph">THE GUCCI PET COLLECTION</p>  
         </div>
       </div>
