54ac9479e0d02dcea508aa2aa49b8d7865f1b203
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-03 14:52:28 +0100

    Section 4

--- gucci-website/src/home-page/Section-4.js
+++ gucci-website/src/home-page/Section-4.js
@@ -3,7 +3,14 @@ import "../styling/section-four.css"
 
 function SectionFour(){
   return (
-    <div className="section-four">Hey</div>
+    <div className="section-four">
+      <h4>WHAT'S NEW</h4>
+      <div>
+        <div className="left-row">Left</div>
+        <div className="center-row">Center</div>
+        <div className="right-row">Right</div>
+      </div>
+    </div>
   )
 }
 
