1410bccb6d7d59bb8fc12b8d46c9cfe46edd798e
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-03 15:11:41 +0100

    Section 4 styling

--- gucci-website/src/home-page/Section-4.js
+++ gucci-website/src/home-page/Section-4.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "../styling/section-four.css"
+import gucciImage from "../model/oladimeji-odunsi-Wu3yqve2gnc-unsplash.jpg"
 
 function SectionFour(){
   return (
     <div className="section-four">
       <h4>WHAT'S NEW</h4>
       <div>
-        <div className="left-row">Left</div>
-        <div className="center-row">Center</div>
-        <div className="right-row">Right</div>
+        <div className="column">
+          <img className="column-image" src={gucciImage} alt="gucci img" />
+          <p className="column-paragraph">GUCCI GOOD GAME COLLECTION</p>
+        </div>
+        <div className="column">
+          <img className="column-image" src={gucciImage} alt="gucci img" />
+          <p className="column-paragraph">GUCCI HA HA HA</p>  
+        </div>
+        <div className="column">
+          <img className="column-image" src={gucciImage} alt="gucci img" />
+          <p className="column-paragraph">THE GUCCI PET COLLECTION</p>  
+        </div>
       </div>
+
+      <a className="view-all-new" href="www.gucci.com">VIEW ALL</a>
     </div>
   )
 }
