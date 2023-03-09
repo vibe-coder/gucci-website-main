334e755cf10ac57388bc7c2d1d21cca5578cfdc6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-09 22:09:38 +0100

    About Section 2 styling

--- gucci-website/src/About-page/AboutSection2.js
+++ gucci-website/src/About-page/AboutSection2.js
@@ -1,4 +1,5 @@
 import "./about-styling/about-section-2.css"
+import BagOne from "../model/wiser-by-the-mile-OFHkPCkhYEY-unsplash.jpg"
 
 export default function AboutSection2(){
   return(
@@ -6,7 +7,34 @@ export default function AboutSection2(){
       <h4 className="bamboo-header">GUCCI BAMBOO 1947</h4>
 
       <div className="bamboo-collection-wrapper">
-        <div className="left-column"><p>Hey</p></div>
+        <div className="left-column">
+
+          <div className="bag-section">
+            <div className="bag-1-wrapper">
+              <img className="bag-1" src={BagOne} alt="Bag One" />
+              <p>Bamboo large backpack</p>
+            </div>
+
+            <div className="bag-2-wrapper">
+              <img className="bag-1" src={BagOne} alt="Bag One" />
+              <p>Bamboo large backpack</p>
+            </div>
+          </div>
+
+
+          <div className="text-section">
+            <h3>Carry a Piece of History</h3>
+            <p>In post-war 1947, when traditional materials were 
+              <br/>difficult to find, Guccio Gucci and the House's
+              <br/>Florentine artisans decided to use the lightweight,
+              <br/>durable bamboo for the handle of a new bag, forging
+              <br/>an instant emblem.
+            </p>
+            <a href="www.gucci.com">VIEW COLLECTION</a>
+          </div>
+        </div>
+
+        {/* Right Section */}
         <div className="right-column"><p>Hey Right</p></div>
       </div>
     </div>
