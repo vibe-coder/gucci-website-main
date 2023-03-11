a3990afeed3ff06f2288ce65a8d681673a106f66
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-11 22:56:45 +0100

    Setting up About Section 3

--- gucci-website/src/About-page/AboutContainer.js
+++ gucci-website/src/About-page/AboutContainer.js
@@ -1,11 +1,13 @@
 import AboutSection1 from "./AboutSection1"
 import AboutSection2 from "./AboutSection2"
+import AboutSection3 from "./AboutSection3"
 
 export default function AboutContainer(){
   return (
     <>
       <AboutSection1/>
       <AboutSection2/>
+      <AboutSection3/>
     </>
   )
 }
---
+++ gucci-website/src/About-page/AboutSection3.js
@@ -0,0 +1,9 @@
+import React  from "react"
+import "./about-styling/about-section-3.css"
+
+
+export default function AboutSection3(){
+  return (
+    <div className="about-section-3">Hey</div>
+  )
+}
