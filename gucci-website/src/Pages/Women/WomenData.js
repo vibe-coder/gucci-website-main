f5ab1e67d4ac38994ed7ae884823d2eaa989c91f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-13 21:30:12 +0100

    Setting up women produst page

---
+++ gucci-website/src/Pages/Women/WomenData.js
@@ -0,0 +1,125 @@
+const WomenData = [
+
+  {
+    image:"image-1",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-2",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-3",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-4",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-5",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-6",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-7",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-8",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-9",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-10",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-11",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-12",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-13",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-14",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-15",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-16",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-17",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-18",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-19",
+    name:"product-name",
+    price:"product-price"
+  },
+
+  {
+    image:"image-20",
+    name:"product-name",
+    price:"product-price"
+  }
+
+]
+
+export default WomenData 
