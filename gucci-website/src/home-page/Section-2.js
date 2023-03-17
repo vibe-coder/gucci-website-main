e0bf3ea185454929685f0eb866eabb56a2d3d1f6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-17 02:30:03 +0100

    Final Touches

--- gucci-website/src/home-page/Section-2.js
+++ gucci-website/src/home-page/Section-2.js
@@ -1,7 +1,9 @@
 import React from "react";
 import  "../styling/section-two.css"
 import modelOne from "../model/oladimeji-odunsi-aU_eOcelLhQ-unsplash.jpg"
-import modelTwo from "../model/alireza-dolati-OVS3rqXq9gg-unsplash.jpg"
+import modelTwo from "../model/pexels-alessio-cesario-1906815.jpg"
+import modelThree from "../model/pexels-luca-nardone-3651597.jpg"
+import modelFour from "../model/pexels-drew-williams-3568518.jpg"
 
 function SectionTwo(){
   return(
@@ -27,12 +29,12 @@ function SectionTwo(){
 
       <div className="middle-section"> 
       <div className="center">
-          <img src={modelOne}  alt="model one"/>
+          <img src={modelThree}  alt="model one"/>
           <p>FOR MEN</p>
         </div>
 
         <div className="right">
-        <img src={modelTwo}  alt="model one"/>
+        <img src={modelFour}  alt="model one"/>
         </div>
         
         
