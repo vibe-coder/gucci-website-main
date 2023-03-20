6c673fc85a89be8d709bc269be471b4203f63750
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-20 11:31:18 +0100

    Adjusting men data

--- gucci-website/src/Pages/Men/MenData.js
+++ gucci-website/src/Pages/Men/MenData.js
@@ -3,140 +3,140 @@ const MenData = [
   {
     key:"1",
     image:"./men-prodcut-Image/m-image-1.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 950"
   },
 
   {
     key:"2",
     image:"./men-prodcut-Image/m-image-2.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 3,600"
   },
 
   {
     key:"3",
     image:"./men-prodcut-Image/m-image-3.jpg",
-    name:"product-name-3",
+    name:"Winter jacket",
     price:"$ 1,800"
   },
 
   {
     key:"4",
     image:"./men-prodcut-Image/m-image-4.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 3,600"
   },
 
   {
     key:"5",
     image:"./men-prodcut-Image/m-image-5.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 3,900"
   },
 
   {
     key:"6",
     image:"./men-prodcut-Image/m-image-6.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 4,850"
   },
 
   {
     key:"7",
     image:"./men-prodcut-Image/m-image-7.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 7,350"
   },
 
   {
     key:"8",
     image:"./men-prodcut-Image/m-image-8.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 6,685"
   },
 
   {
     key:"9",
     image:"./men-prodcut-Image/m-image-9.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 4,150"
   },
 
   {
     key:"10",
     image:"./men-prodcut-Image/m-image-10.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 3,100"
   },
 
   {
     key:"11",
     image:"./men-prodcut-Image/m-image-11.jpg",
-    name:"product-name",
+    name:"Winter jacket",
     price:"$ 4,550"
   },
 
   {
     key:"12",
     image:"./men-prodcut-Image/m-image-12.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 4,650"
   },
 
   {
     key:"13",
     image:"./men-prodcut-Image/m-image-13.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 1,625"
   },
 
   {
     key:"14",
     image:"./men-prodcut-Image/m-image-14.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 6,450"
   },
 
   {
     key:"15",
     image:"./men-prodcut-Image/m-image-15.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"2,150"
   },
 
   {
     key:"16",
     image:"./men-prodcut-Image/m-image-16.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 4,900"
   },
 
   {
     key:"17",
     image:"./men-prodcut-Image/m-image-17.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 5,850"
   },
 
   {
     key:"18",
     image:"./men-prodcut-Image/m-image-18.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 3,665"
   },
 
   {
     key:"19",
     image:"./men-prodcut-Image/m-image-19.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 4,650"
   },
 
   {
     key:"20",
     image:"./men-prodcut-Image/m-image-20.jpg",
-    name:"product-name",
+    name:"Pastel casual wear",
     price:"$ 5,550"
   }
 
